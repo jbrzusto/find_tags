d7b52f6cd52b6742fa9778fb986cd37c1d3d7672
john <jbrzusto@fastmail.fm>
2013-10-07 11:39:43 +0000

    ignore with warning faulty tag database file lines

--- Tag_Database.cpp
+++ Tag_Database.cpp
@@ -14,15 +14,16 @@ Tag_Database::Tag_Database(string filename) {
     inf.getline(buf, MAX_LINE_SIZE);
     if (inf.gcount() == 0)
       break;
+    ++num_lines;
+
     char proj[MAX_LINE_SIZE+1], filename[MAX_LINE_SIZE];
     int id;
     float freq_MHz, fcd_freq, gaps[4], dfreq, g1sd, g2sd, g3sd, bisd, dfreqsd;
     int num_par = sscanf(buf, "\"%[^\"]\",%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,\"%[^\"]\"", proj, &id, &freq_MHz, &fcd_freq, &gaps[0], &gaps[1], &gaps[2], &gaps[3], &dfreq, &g1sd, &g2sd, &g3sd, &bisd, &dfreqsd, filename);
     if (num_par < 15) {
-      std::cerr << "error at line " << num_lines << " with only " << num_par << " parameters parsed.\n";
-      throw std::runtime_error("Tag file line incomplete or corrupt\n");
+      std::cerr << "Ignoring faulty line " << num_lines << " of tag database file:\n   " << buf << std::endl;
+      continue;
     }
-    ++ num_lines;
     Nominal_Frequency_kHz nom_freq = Freq_Setting::as_Nominal_Frequency_kHz(freq_MHz);
     // convert gaps to seconds
     for (int i=0; i < 3; ++i)
@@ -58,5 +59,5 @@ Known_Tag *
 Tag_Database::get_tag(Nominal_Frequency_kHz freq, Tag_ID id) {
   return & tags[freq][id];
 };
-  
+
 
