c2b427ee343762814cb0288b68a459197f4c1e4b
john brzustowski <jbrzusto@fastmail.fm>
2016-02-22 18:41:38 +0000

    working tag deletion

--- test7.cpp
+++ test7.cpp
@@ -1,11 +1,13 @@
 #include <set>
 #include <map>
 #include <iostream>
+#include <fstream>
 #include <stdexcept>
 #include <cmath>
 #include <memory>
+#include <vector>
 #include <unordered_map>
-
+#include <string>
 
 class Node;
 typedef double Point;
@@ -14,10 +16,6 @@ typedef short Phase;
 typedef std::pair < TagID, Phase > TagPhase;
 typedef std::unordered_multimap < TagID, Phase > TagPhaseSet;
 
-TagPhase TP(TagID t, Phase p) {
-  return std::make_pair(t, p);
-};
-
 
 template<class CharType, class CharTraits>
 std::basic_ostream<CharType, CharTraits> &operator<<
@@ -25,6 +23,15 @@ std::basic_ostream<CharType, CharTraits> &operator<<
 {
   if (value.second >= 0)
     return stream << "# " << value.first << " (" << value.second << ") ";
+  return stream;
+};
+
+template<class CharType, class CharTraits>
+std::basic_ostream<CharType, CharTraits> &operator<<
+(std::basic_ostream<CharType, CharTraits> &stream, TagPhaseSet const& value)
+{
+  for (auto i = value.begin(); i != value.end(); ++i)
+    stream << "\\n" << (*i);
   return stream;
 };
 
@@ -105,6 +112,10 @@ public:
         return;
       }
     }
+  };
+
+  int count(TagID id) {
+    return s.count(id);
   };
 
   int count(TagPhase p) {
@@ -195,7 +206,7 @@ std::set < Set * > Set::allSets = std::set < Set * > ();
 struct hashSet {
   // hashing function used in DFA::setToNode
   size_t operator() (const Set * x) const {
-    return x->hash;
+    return  x ? x->hash : 0;
   };
 };
 
@@ -269,6 +280,10 @@ protected:
   void _init() {
     useCount = 0;
     label = numNodes++;
+    if (_empty) {
+      e.insert(std::make_pair(-1.0 / 0.0, _empty));
+      e.insert(std::make_pair( 1.0 / 0.0, _empty));
+    }
   }
 
 public:
@@ -276,6 +291,8 @@ public:
   static void init() {
     Set::init();
     _empty = new Node();
+    //    _empty->e.insert(std::make_pair(-1.0 / 0.0, _empty));
+    //    _empty->e.insert(std::make_pair( 1.0 / 0.0, _empty));
   };
 
   static Node * empty() {
@@ -284,8 +301,6 @@ public:
   
   Node() : s(Set::empty()), e() {
     _init();
-    e.insert(std::make_pair(-1.0 / 0.0, _empty));
-    e.insert(std::make_pair( 1.0 / 0.0, _empty));
   };
 
   Node(const Node &n) : s(n.s), e(n.e) { 
@@ -303,8 +318,8 @@ public:
   };
 
   
-  void add (Point b1, Point b2, TagPhase p);
-  void remove (Point b1, Point b2, TagPhase p);
+  void add (Point lo, Point hi, TagPhase p);
+  void remove (Point lo, Point hi, TagPhase p);
 
   void dump(bool skipEdges=false) {
     std::cout << "Node: " << label << " has " << e.size() << " entries in edge map:\n";
@@ -330,6 +345,10 @@ Node * Node::_empty = 0;
 struct SetEqual {
   // comparison function used in DFA::setToNode
   bool operator() ( const Set * x1, const Set * x2 ) const {
+    if (! x1 && ! x2) 
+      return true;
+    if (!x1 || ! x2)
+      return false;
     return x1->s == x2->s; // fixme slow direct comparison
   };
 };
@@ -337,17 +356,30 @@ struct SetEqual {
 
 class Graph {
   Node * root;
+  std::string vizPrefix;
+  int numViz;
 
 public:
 
-  // map from sets to nodes
-  std::unordered_map < Set *, Node *, hashSet, SetEqual > setToNode; 
 
   // void insert (DFA_Node *n, const ID &t); // insert tagphase to non-root node
   // void erase (DFA_Node *n, const ID &t); // erase tagphase from non-root node
-  void insert_rec (Node *n, Point lo, Point hi, TagPhase tFrom, TagPhase tTo); // recursively insert a transition from tFrom to tTo
-  void erase_rec (Node *n, Point lo, Point hi, TagPhase tFrom, TagPhase tTo); // recursively erase all transitions from tFrom to tTo
+  // void insertRec (Node *n, Point lo, Point hi, TagPhase tFrom, TagPhase tTo); // recursively insert a transition from tFrom to tTo
+  // void eraseRec (Node *n, Point lo, Point hi, TagPhase tFrom, TagPhase tTo); // recursively erase all transitions from tFrom to tTo
   Node * advance (Node *n, Point dt);
+  Graph(std::string vizPrefix) : vizPrefix(vizPrefix), numViz(0), setToNode(100) {
+    root = new Node();
+    mapSet(Set::empty(), Node::empty());
+    mapSet(0, root);
+  };
+  void dump () {
+    root->dump();
+  };
+
+
+
+  // map from sets to nodes
+  std::unordered_map < Set *, Node *, hashSet, SetEqual > setToNode; 
 
   void removeEdge (Node * head, Point b, Node * tail); // remove edge at point b from head to tail
   void augmentEdge (Node * head, Point b, Node * tail, TagPhase p); // existing edge from head at point b will now go to node for set union(tail->s, {p})
@@ -364,11 +396,6 @@ public:
   void unmapSet ( Set * s) {
     if (s != Set::empty())
       setToNode.erase(s);
-  };
-
-  Graph() : setToNode(100) {
-    root = new Node();
-    mapSet(Set::empty(), Node::empty());
   };
 
   Node * nodeForSet (Set * s) {
@@ -412,32 +439,39 @@ public:
     return nn;
   };
 
-  void dump () {
-    root->dump();
-  };
-
 
   Node * addEdge (Node * head, Point b, Set *s, TagPhase p) {
     // add edge from head at point b to node for set union(s, {p}); return pointer to tail node
   };
 
   void insert (const TagPhase &t) {
-    root->s->augment(t);
+    root->s = root->s->augment(t);
     // note: we don't remap root in setToNode, since we
     // never try to lookup the root node from its set.
   };
 
   void erase (const TagPhase &t) {
-    root->s->reduce(t);
+    root->s = root->s->reduce(t);
     // note: we don't remap root in setToNode
   };
 
-  void insert (Point b1, Point b2, TagPhase p) {
-    insert (root, b1, b2, p);
+  void insert (Point lo, Point hi, TagPhase p) {
+    insert (root, lo, hi, p);
   };
 
-  void erase (Point b1, Point b2, TagPhase p) {
-    erase(root, b1, b2, p);
+  void erase (Point lo, Point hi, TagPhase p) {
+    erase(root, lo, hi, p);
+  };
+
+  bool hasEdge ( Node *n, Point b, TagPhase p) {
+    // check whether there is already an edge from
+    // node n at point b to a set with tagPhase p
+    auto i = n->e.lower_bound(b);
+    if (i->first != b)
+      return false;
+    if (i->second->s->count(p))
+      return true;
+    return false;
   };
 
   Node::Edges::iterator ensureEdge ( Node *n, Point b) {
@@ -466,6 +500,10 @@ public:
   void unlinkNode (Node *n) {
     if (n->unlink() && n != Node::empty()) {
       unmapSet(n->s);
+      // unlink tail nodes
+      for (auto i = n->e.begin(); i != n->e.end(); ++i)
+        if (i->second != Node::empty())
+          unlinkNode(i->second);
       n->drop();
     };
   };
@@ -557,41 +595,85 @@ public:
     };
   };
 
-  void insert (Node *n, Point b1, Point b2, TagPhase p)
+  void insert (Node *n, Point lo, Point hi, TagPhase p)
   {
     // From the node at n, add appropriate edges to other nodes
-    // given that the segment [b1, b2] is being augmented by p.
+    // given that the segment [lo, hi] is being augmented by p.
 
-    ensureEdge(n, b2);  // ensure there is an edge from n at b2 to the
+    ensureEdge(n, hi);  // ensure there is an edge from n at hi to the
                         // current node for that point
 
-    auto i = ensureEdge(n, b1); // ensure there is an edge from n at
-                                // b1 to the current node for that
+    auto i = ensureEdge(n, lo); // ensure there is an edge from n at
+                                // lo to the current node for that
                                 // point
 
-    // From the b1 to the last existing endpoint in [b1, b2), augment
+    // From the lo to the last existing endpoint in [lo, hi), augment
     // each edge by p.
 
-    while (i->first < b2) {
+    while (i->first < hi) {
       augmentEdge(i, p);
       ++i;
     }
   };
     
-  void erase (Node *n, Point b1, Point b2, TagPhase p) {
-    // for any edges from n at points in the range [b1, b2]
+  void insertRec (Node *n, Point lo, Point hi, TagPhase tFrom, TagPhase tTo) {
+    // recursively insert a transition from tFrom to tTo because this
+    // is a DAG, rather than a tree, a given node might already have
+    // been visited by depth-first search, so we don't continue the
+    // recursion of the given node already has the transition
+    auto id = tFrom.first;
+
+    if (hasEdge(n, lo, tTo))
+      return;
+    for(auto i = n->e.begin(); i != n->e.end(); ++i) {
+      if (i->second->s->count(id)) {
+        insertRec(i->second, lo, hi, tFrom, tTo);
+      }
+    }
+      // possibly add edge from this node
+    if (n->s->count(tFrom))
+      insert(n, lo, hi, tTo);
+  };
+
+  void erase (Node *n, Point lo, Point hi, TagPhase tp) {
+    // for any edges from n at points in the range [lo, hi]
     // remove p from the tail node.
-    // If edges at b1 and/or b2 become redundant after 
+    // If edges at lo and/or hi become redundant after 
     // this, remove them.
 
-    auto i = n->e.lower_bound(b1);
+    auto i = n->e.lower_bound(lo);
     auto j = i; // save value for later
-    while (i->first <= b2) {
-      reduceEdge(i, p);
+    while (i->first <= hi) {
+      reduceEdge(i, tp);
       ++i;
     }
-    dropEdgeIfExtra(n, --i); // rightmost edge
-    dropEdgeIfExtra(n, j);  // leftmost edge
+    --i;
+    if (std::isfinite(i->first))
+      dropEdgeIfExtra(n, i); // rightmost edge
+    if (std::isfinite(j->first))
+      dropEdgeIfExtra(n, j);  // leftmost edge
+  };
+
+  void eraseRec (Node *n, Point lo, Point hi, TagPhase tpFrom, TagPhase tpTo) {
+    // recursively erase all transitions between tpFrom and tpTo for
+    // the range lo, hi
+    auto id = tpFrom.first;
+    bool here = n->s->count(tpFrom) > 0;
+    bool usedHere = true;
+    for(auto i = n->e.begin(); i != n->e.end(); ) {
+      auto j = i;
+      ++j;
+      if (i->second->s->count(id)) {
+        // recurse to any child node that has this tag ID in
+        // its set, because we might eventually reach a node with tpFrom
+        eraseRec(i->second, lo, hi, tpFrom, tpTo);
+        if (here && i->second->s->count(tpTo))
+          usedHere = true;
+      }
+      i = j;
+    }
+    if (usedHere)
+      erase(n, lo, hi, tpTo);
   };
 
   void dumpSetToNode() {
@@ -613,6 +695,11 @@ public:
       if (! i->second) {
         std::cout << "Null Node Ptr for set:" << std::endl;
         i->first->dump();
+      } else if (! i->first) {
+        if (i->second != root) {
+          std::cout << "Non-root Node mapped by null set *: " << std::endl;
+          i->second->dump();
+        }
       } else if (i->second->s != i->first) {
         std::cout << "Invalid setToNode map:" << std::endl;
         dumpSetToNode();
@@ -621,100 +708,211 @@ public:
     }
     std::cout << "setToNode is okay" << std::endl;
   };
+
+  void  viz() {
+    // dump graph in the "dot" format for the graphviz package
+    // These .gv files can be converted to nice svg plots like so:
+    // for x in *.gv; do dot -Tsvg $x > ${x/gv/svg}; done
+
+    std::ofstream out(vizPrefix + std::to_string(++numViz) + ".gv");
+
+    out << "digraph TEST {\n";
+
+    // generate node symbols and labels
+    for (auto i = setToNode.begin(); i != setToNode.end(); ++i) {
+      // define the node, labelled with its TagPhase set
+      out << "a" << i->second->label <<
+        "[label=\"a" << i->second->label << i->second->s->s << "\"];\n";
+
+      // define each edge, labelled by its interval
+      for(auto j = i->second->e.begin(); j != i->second->e.end(); ++j) {
+        Set * m = j->second->s;
+        if (m != Set::empty()) {
+          auto n = setToNode.find(m);
+          if (n != setToNode.end()) {
+            auto k = j;
+            ++k;
+            out << "a" << i->second->label << " -> a" << (n->second->label) << "[label = \"["
+                << j->first << "," << k->first << "]\"];\n";
+          }
+        }
+      }
+    }
+    out << "}\n";
+  };
+
+  void addTag(TagID id, std::vector < double > gaps, double tol, double timeFuzz, double maxTime) {
+    // add the repeated sequence of gaps from a tag, with fractional
+    // tolerance tol, starting at phase 0, until adding the next gap
+    // would exceed a total elapsed time of maxTime.  The gap is set
+    // to an interval given by gap * (1-tol), gap * (1 + tol)
+
+    // NB: gap 0 takes the tag from phase 0 to phase 1, etc.
+    int n = gaps.size();
+    int p = 0;
+    double t = 0;
+    TagPhase last(id, p);
+    insert(last);
+    for(;;) {
+      double g = gaps[p % n];
+      t += g;
+      if (t > maxTime)
+        break;
+      ++p;
+      TagPhase next(id, p);
+      insertRec(root, std::min(g - tol, g * (1 - timeFuzz)), std::max(g + tol, g * (1 + timeFuzz)), last, next);
+      last = next;
+    };
+  };    
+
+  void delTag(TagID id, std::vector < double > gaps, double tol, double timeFuzz, double maxTime) {
+    // remove the tag which was added with given gaps, tol, and maxTime
+
+    int n = gaps.size();
+    int p = 0;
+    double t = 0;
+    for(;;) {
+      t += gaps[p % n];
+      if (t > maxTime)
+        break;
+      ++p;
+    }      
+
+    while(p > 0) {
+      --p;
+      double g = gaps[p % n];
+      eraseRec(root, std::min(g - tol, g * (1 - timeFuzz)), std::max(g + tol, g * (1 + timeFuzz)), TagPhase(id, p), TagPhase(id, p+1));
+#ifdef DEBUG
+      viz();
+#endif
+    }
+    erase(TagPhase(id, 0));
+#ifdef DEBUG
+    viz();
+#endif
+  };
+
+
+
 };
 
 main (int argc, char * argv[] ) {
   Node::init();
-  Graph g;
+  Graph g("test");
 
-  g.insert(5, 10, TP(1000, 1));
-  // g.dump();
-  Set::dumpAll();
+  int n = 0;
 
-  g.insert(7, 12, TP(1001, 1));
+
+  g.insert(5, 10, TagPhase(1000, 1));
+  // g.dump();
+  //  Set::dumpAll();
+
+  g.insert(7, 12, TagPhase(1001, 1));
   // g.dump();
   
-  g.insert(4, 9, TP(1002, 1));
+  g.insert(4, 9, TagPhase(1002, 1));
   // g.dump();
   
-  g.insert(3, 13, TP(1003, 1));
+  g.insert(3, 13, TagPhase(1003, 1));
   // g.dump();
 
-  Set::dumpAll();
+  //  Set::dumpAll();
 
-  g.insert(7, 9, TP(1004, 1));
+  g.insert(7, 9, TagPhase(1004, 1));
   // g.dump();
   
-  g.insert(1, 2, TP(1005, 1));
+  g.insert(1, 2, TagPhase(1005, 1));
   // g.dump();
   
-  g.insert(0, 1, TP(1006, 1));
+  g.insert(0, 1, TagPhase(1006, 1));
   // g.dump();
   
-  g.insert(14, 16, TP(1007, 1));
+  g.insert(14, 16, TagPhase(1007, 1));
   // g.dump();
   
-  g.insert(14, 18, TP(1008, 1));
+  g.insert(14, 18, TagPhase(1008, 1));
   // g.dump();
   
-  g.insert(12, 19, TP(1009, 1));
+  g.insert(12, 19, TagPhase(1009, 1));
   g.dump();
 
   Set::dumpAll();
   g.validateSetToNode();
+
+
+  g.viz();
+
+  // New tag with ID 4 and gaps 6.7, 4.1, 4.3, BI=110
+
+  std::vector < double > T2;
+  T2.push_back(4.1);  
+  T2.push_back(4.3);
+  T2.push_back(6.7);
+  T2.push_back(110);
+
+  double tol = 0.25;
+  double timeFuzz = 0.05;
+
+  g.addTag(4, T2, tol, timeFuzz, 500);
+
+  g.viz();
 
   //-------------------------------------------------------------
 
-  g.erase(5, 10, TP(1000, 1));
-  g.dump();
+  g.erase(5, 10, TagPhase(1000, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
 
-  g.erase(14, 16, TP(1007, 1));
-  g.dump();
+  g.erase(14, 16, TagPhase(1007, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
   
-  g.erase(3, 13, TP(1003, 1));
-  g.dump();
+  g.erase(3, 13, TagPhase(1003, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
   
-  g.erase(1, 2, TP(1005, 1));
-  g.dump();
+  g.erase(1, 2, TagPhase(1005, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
 
-  g.erase(7, 12, TP(1001, 1));
-  g.dump();
+  g.erase(7, 12, TagPhase(1001, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
   
-  g.erase(4, 9, TP(1002, 1));
-  g.dump();
+  g.erase(4, 9, TagPhase(1002, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
 
-  g.erase(7, 9, TP(1004, 1));
-  g.dump();
+  g.erase(7, 9, TagPhase(1004, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
   
-  g.erase(12, 19, TP(1009, 1));
-  g.dump();
+  g.erase(12, 19, TagPhase(1009, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
 
-  g.erase(0, 1, TP(1006, 1));
-  g.dump();
+  g.erase(0, 1, TagPhase(1006, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
   
-  g.erase(14, 18, TP(1008, 1));
-  g.dump();
+  g.erase(14, 18, TagPhase(1008, 1));
+  g.viz();
   Set::dumpAll();
   g.validateSetToNode();
 
-  Set::dumpAll();
+  g.delTag(4, T2, tol, timeFuzz, 500);
+
+  g.viz();
+
+  g.validateSetToNode();
   
 };
