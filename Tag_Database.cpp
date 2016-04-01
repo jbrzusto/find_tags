c233fcb48efa143a6cb896f480fa8a96dfe703bb
john brzustowski <jbrzusto@fastmail.fm>
2016-04-01 15:42:45 +0000

    expect hyphen before version number in Lotek codeset name

--- Tag_Database.cpp
+++ Tag_Database.cpp
@@ -45,10 +45,10 @@ Tag_Database::populate_from_sqlite_file(string filename, bool get_history) {
     double gap = sqlite3_column_double(st, 6) - totalBurst;
     gaps.push_back(gap);
 
-    // extract codeset; "Lotek3"->3, "Lotek4"->4, otherwise 0
+    // extract codeset; "Lotek-3"->3, "Lotek-4"->4, otherwise 0
     short codeSet = 0;
     if (sqlite3_column_bytes(st, 8) == 6)
-      codeSet = sqlite3_column_text(st, 8)[5] - '0';
+      codeSet = sqlite3_column_text(st, 8)[6] - '0';
     short id = sqlite3_column_int(st, 7);
     
     Nominal_Frequency_kHz nom_freq = Freq_Setting::as_Nominal_Frequency_kHz(freq_MHz);
