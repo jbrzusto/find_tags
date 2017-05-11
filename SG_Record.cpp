d89dd792698238d37a7738b29686b7ea36cb07e9
john brzustowski <jbrzusto@fastmail.fm>
2017-05-11 14:31:00 +0000

    check for too-short record before calling sscanf

--- SG_Record.cpp
+++ SG_Record.cpp
@@ -24,7 +24,7 @@ SG_Record::from_buf(char * buf) {
        G,1458001712,44.34021,-66.118733333,21.6
        ts        lat        lon        alt
     */
-    if (4 == sscanf(buf+2, "%lf,%lf,%lf,%lf", &rv.ts, &rv.v.lat, &rv.v.lon, &rv.v.alt)) {
+    if (buf[1] != '\0' && 4 == sscanf(buf+2, "%lf,%lf,%lf,%lf", &rv.ts, &rv.v.lat, &rv.v.lon, &rv.v.alt)) {
       rv.type = GPS;
     }
     break;
@@ -34,7 +34,7 @@ SG_Record::from_buf(char * buf) {
        S,1366227448.192,5,-m,166.376,0,
        is S, timestamp, port_num, param flag, value, return code, other error
     */
-    if (5 == sscanf(buf+2, "%lf,%hd,%[^,],%lf,%d,", &rv.ts, &rv.port, (char *) &rv.v.param_flag, &rv.v.param_value, &rv.v.return_code)) {
+    if (buf[1] != '\0' && 5 == sscanf(buf+2, "%lf,%hd,%[^,],%lf,%d,", &rv.ts, &rv.port, (char *) &rv.v.param_flag, &rv.v.param_value, &rv.v.return_code)) {
       rv.type = PARAM;
     }
     break;
@@ -45,7 +45,7 @@ SG_Record::from_buf(char * buf) {
        which gives the new timestamp, level of correction (roughly 10^-X), residual correction
        Mainly of interest because it provides a timestamp.
     */
-    if (3 == sscanf(buf+2, "%lf,%d,%lf", &rv.ts, &rv.v.clock_level, &rv.v.clock_remaining)) {
+    if (buf[1] != '\0' && 3 == sscanf(buf+2, "%lf,%d,%lf", &rv.ts, &rv.v.clock_level, &rv.v.clock_remaining)) {
       rv.type = CLOCK;
     }
     break;
